4b78753fb90052733b432d3652d7ad2d35c07384
Giovanny Ospina Alzate <gio02078017@gmail.com>
2020-05-14 21:32:00 -0500

    se realiza commit de ejemplo reducer en http

--- redux/http-redux-app/src/redux/store.js
+++ redux/http-redux-app/src/redux/store.js
@@ -1,10 +1,28 @@
 import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducers'
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers = composeWithDevTools({
+    name: 'Redux',
+    realtime: true,
+    trace: true,
+    traceLimit: 20
+  })
+
+  const store = createStore(
+    rootReducer,
+    composeEnhancers(
+      applyMiddleware(logger, thunk)
+    )
+  )
+
+
 
 // Store
 // Almacenamiento de nuestro estado
-const store = createStore(rootReducer, applyMiddleware( logger, thunk))
+//const store = createStore(rootReducer, applyMiddleware( logger, thunk))
 
 export default store
